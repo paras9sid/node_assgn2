e7a4a6e9d68d7d0b0f72f9c25b6dbed8059af0b3
paras9sid <sidharthjainp9@gmail.com>
2023-03-25 13:56:29 +0530

    exp js ins-2 middlewares created and response sent

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
+//package imports
 const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("middleware run");
-  res.send("<h1>express running successfully on browser</h1>");
+//use allows to add us new middleware function
+//route added with app.use - handling routes
+app.use("/", (req, res, next) => {
+  console.log("First middleware");
+  // sending response
+  res.send("<h1>Home</h1>");
+  next();
+});
+app.use("/users", (req, res, next) => {
+  console.log("Another middleware");
+  // sending response
+  // res.send("<h1>came to user route</h1>");
 });
 
-app.listen(6000);
+app.listen(3000); //shortcut for server removing above 2 lines
